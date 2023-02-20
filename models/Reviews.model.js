50ab7629c2b034b7f4eef114facea859db67ca64
Francisco Manuel Pereira <70414789+FranciscoManuelPereira@users.noreply.github.com>
2023-02-20 11:37:07 +0000

    models

---
+++ models/Reviews.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const reviewsSchema = new Schema (
+    {
+        author: [{ type: Schema.Types.ObjectId, ref: "User"}],
+        receiver: [{ type: Schema.Types.ObjectId, ref: "User"}],
+
+        content: {
+            type: String,
+        },
+
+        rete: {
+            type: Number,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+const Reviews = model("Reviews", reviewsSchema);
+
+module.exports = Reviews;
