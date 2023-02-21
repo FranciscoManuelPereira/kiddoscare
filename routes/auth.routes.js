d22907257cdfbaca50ddd4e7994ad7f1a9687995
Joanadgomes <joanadiogog@gmail.com>
2023-02-21 15:30:53 +0000

    Co-authored-by: Francisco Manuel Pereira <FranciscoManuelPereira@users.noreply.github.com>

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -165,17 +165,3 @@ router.get("/logout", isLoggedIn, (req, res) => {
 
 module.exports = router;
 
-//Home ("/") if isLoggedIn (login and signup button) disable
-
-/* let login = document.querySelector(".signup-btn");
-let logout = document.querySelector(".logout-btn");
-
-login.addEventListener('click', function(e) {
-  this.classList.add('is-hidden')
-  logout.classList.remove('is-hidden');
-})
-
-logout.addEventListener('click', function(e) {
-  this.classList.add('is-hidden');
-  login.classList.remove('is-hidden');
-}) */
